1516d7c24b89b27144f4c2dc8fff73c809c6a8d9
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-18 15:51:27 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+
+class Dog
+  def name=(doggy)
+    @me = doggy
+  end
+  def name
+    @me
+  end
+  def breed=(heritage)
+    @liniage = heritage
+  end
+  def breed
+    @liniage
+  end
+end
+
+    
